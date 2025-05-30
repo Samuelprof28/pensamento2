b0a954fa297d371a1f9d18bd4b3c923579ae0895
Samuelprof28 <samuel.pereira.silva28@escola.pr.gov.br>
2025-05-30 09:35:57 -0300

    Add files via upload

---
+++ operações.js
@@ -0,0 +1,39 @@
+// Subtração
+
+let a = -2;
+let b = 10;
+
+let subtracao = a - b;
+console.log("O resultado da subtração é: " + subtracao);
+
+// Multiplicação
+
+let c = 8;
+let d = 6;
+
+let multiplicacao = (c * d) + (d - c);
+console.log("O resultado da multiplicação é: " + multiplicacao);
+
+// Divisão
+
+let e = 4;
+let f = 50;
+
+let divisao = f / e;
+console.log("O resultado da divisão é: " + divisao);
+
+// Mais operações
+
+let comprovante = subtracao + multiplicacao + divisao;
+console.log("O comprovante de todas as somas é: " + comprovante);
+
+let comprovante2 = (divisao * subtracao) + multiplicacao;
+console.log("O resultado de comprovante2: " + comprovante2);
+
+ // Contador
+
+ let contador = 10;
+ contador++;
+ console.log("O incremento final é: " + contador);
+
+
---
+++ operações2.js
@@ -0,0 +1,29 @@
+// contador negativo
+
+let contador = 100;
+contador--;
+
+console.log("O resultado do decremento é: " + contador);
+
+// interpolação
+
+const nome = "Jairo";
+console.log(`Olá, ${nome}! Bem vindo ao nosso sistema.`);
+
+// interpolação com operações matemáticas
+
+const a = 5;
+const b = 3;
+
+console.log(`A soma de ${a} + ${b} é ${a+b}.`);
+
+// interpolação dentro de um objeto
+
+const usuario = {
+    nome: "Cleisson Oswaldo Robert da Cruz",
+    idade: 30,
+    cidade: "Rio de Janeiro"
+};
+
+console.log(`O cliente ${usuario.nome} possui ${usuario.idade} anos e mora no ${usuario.cidade}.`);
+
